898ff37c78ce393955ee5ec30780c523f39cae65
Garneauma <marcandre.garneau@servicecanada.gc.ca>
2025-09-08 15:40:58 -0400

    Patch - Report page: adding edge case workaround for hgroup (#14)

--- assets/oag.js
+++ assets/oag.js
@@ -1,3 +1,31 @@
 /*
  * OAG - custom JS
  */
+
+// Temporary workaround to move the report type label from the metadata section to the H1
+(function(document) {
+"use strict";
+const reportTypeLabel = document.querySelector('dd.report-type-label');
+const reportHeading = document.querySelector('h1#wb-cont');
+if (reportTypeLabel && reportHeading) {
+	reportHeading.removeAttribute('id'); // Remove id from H1 if present
+
+	// Create hgroup with id 'wb-cont'
+	const hgroup = document.createElement('hgroup');
+	hgroup.id = 'wb-cont';
+
+	// Create paragraph with label
+	const p = document.createElement('p');
+	const span = document.createElement('span');
+	span.className = 'label label-primary';
+	span.textContent = reportTypeLabel.textContent;
+	p.appendChild(span);
+
+	// Move H1 into hgroup
+	hgroup.appendChild(p);
+	hgroup.appendChild(reportHeading.cloneNode(true));
+
+	// Replace original H1 with hgroup
+	reportHeading.parentNode.replaceChild(hgroup, reportHeading);
+}
+})(document);
